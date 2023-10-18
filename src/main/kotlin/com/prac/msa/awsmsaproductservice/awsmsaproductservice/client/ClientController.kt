3763a224da7be2e2b8c789bbf20cf69119b30b02
burger <burger@burgerui-MacBookPro.local>
2023-10-18 21:07:04 +0900

    feignClient Controller 경로 변경

--- src/main/kotlin/com/prac/msa/awsmsaproductservice/awsmsaproductservice/client/ClientController.kt
+++ src/main/kotlin/com/prac/msa/awsmsaproductservice/awsmsaproductservice/client/ClientController.kt
@@ -1,23 +1,21 @@
 package com.prac.msa.awsmsaproductservice.awsmsaproductservice.client
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/products")
 class ClientController(
     private val clientService: ClientService
 ) {
 
-    @GetMapping("/products/{productId}/exists")
+    @GetMapping("/{productId}/exists")
     fun isProductExists(
         @PathVariable productId: Long
     ): Boolean {
         return clientService.isProductExists(productId)
     }
 
-    @GetMapping("/products/name")
+    @GetMapping("/name")
     fun getProductNamesByProductIds(
         @RequestParam productIds: List<Long>
     ): List<Pair<Long, String>> {
